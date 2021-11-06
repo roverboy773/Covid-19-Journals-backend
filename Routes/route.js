620b4cfaf5814b337a26dcd3a3bd7cb7cda7938c
Pranjal <71340514+roverboy773@users.noreply.github.com>
2021-11-06 21:31:36 +0530

    .

--- Routes/route.js
+++ Routes/route.js
@@ -14,7 +14,7 @@
 
 function routes(app){
   
-    app.get('/journals',blogRoutes().getAllBlogs)
+    app.get('/allJournals',blogRoutes().getAllBlogs)
     app.get('/show-Blog/:uuid',blogRoutes().getIndiBlog)
     app.post('/create-blog',blogRoutes().createBlog)
     app.post('/update-views/:uuid',blogRoutes().updateViews)
